1a9df5102e1d26e937540da2228cf39f971e4ad8
psung4 <72226880+psung4@users.noreply.github.com>
2025-08-25 14:50:53 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -42,9 +42,9 @@ END AS AOV_bucket
     END as itacs_bucket
 
 
-, CASE when cf5.loan_type = 'affirm_go_v3' then 'PI4' 
-         when cf5.loan_type = 'classic' and cf.apr > 0 then 'IB'
-         when cf5.loan_type = 'classic' and cf.apr = 0 then '0pct' end as loan_type_checkout, 
+, CASE when cfv5.loan_type = 'affirm_go_v3' then 'PI4' 
+         when cfv5.loan_type = 'classic' and cf.apr > 0 then 'IB'
+         when cfv5.loan_type = 'classic' and cf.apr = 0 then '0pct' end as loan_type_checkout, 
  
 , count(distinct cfv5.checkout_ari) as checkouts
 , count(distinct case when cfv5.is_login_authenticated = 1 then cfv5.checkout_ari end) as authenticated
